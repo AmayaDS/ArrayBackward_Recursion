68da3dc9edc3cb8207f2627ec7a940c592fdf004
DESKTOP-SIIUHK0\hp <goathamiedesilva@gmail.com>
2023-09-18 22:26:32 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/*public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        System.out.println("\nArray Printed Backward:");
+        printArrayBackward(arr);
+    }
+
+    // Method to print the array forward
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Method to print the array backward
+    static void printArrayBackward(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}*/
+
+//using recursion
